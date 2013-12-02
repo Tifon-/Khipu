0c1fe7350a241386bbe0bc15d8a1d438de95f14a
Nicolás Moncada <nicolas.moncada@tifon.cl>
2013-12-02 18:35:30 -0300

    Se cambia versión biblioteca
    Se actualiza de 1.1.3 a 1.2

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Khipu
 
-Versión Biblioteca: 1.1.3
+Versión Biblioteca: 1.2
 
 Versión API Khipu: 1.2
 
--- src/Khipu.php
+++ src/Khipu.php
@@ -26,7 +26,7 @@ class Khipu
   /**
    * Version
    */
-  const VERSION = '1.1.3';
+  const VERSION = '1.2';
 
   /**
    * Corresponde a la ID del cobrador.
